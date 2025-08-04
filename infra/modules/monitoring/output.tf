e091a902a5589d8fe64b3114afa49257bd7dceae
SAKET KUMAR VISHWAKARMA <svsaket123@gmail.com>
2025-08-04 14:39:42 +0530

    Update output.tf

--- infra/modules/monitoring/output.tf
+++ infra/modules/monitoring/output.tf
@@ -1,8 +1,10 @@
 
 output "instrumentation_key" {
   value = azurerm_application_insights.AppInsights.instrumentation_key
+  sensitive= true
 }
 
 output "connection_string" {
   value = azurerm_application_insights.AppInsights.connection_string
+  sensitive= true
 }
