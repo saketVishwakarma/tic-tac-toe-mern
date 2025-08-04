dc30131f91a8cd0154158bcf6ddabaa1724cce85
saketVishwakarma <svsaket123@gmail.com>
2025-08-04 19:01:20 +0530

    added to prevent deletion if resource group contains resources in provider

--- infra/provider.tf
+++ infra/provider.tf
@@ -3,13 +3,16 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.0"
-      prevent_deletion_if_contains_resources = false
-    }
   }
+}
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
   subscription_id = var.subscription_id
   client_id       = var.client_id
   tenant_id       = var.tenant_id
